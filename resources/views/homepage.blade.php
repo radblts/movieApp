02477cbb8d49ca7bde94fe8db7843eb1aaabfe6f
DxniChid <chidiroglou.danai06@gmail.com>
2025-02-05 08:36:23 +0100

    Saving CSS

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,6 +9,29 @@
         
 </head>
 <body>
+<nav class="menu">
+  <ol>
+    <li class="menu-item"><a href="#0">Home</a></li>
+    <li class="menu-item"><a href="#0">About</a></li>
+    <li class="menu-item">
+      <a href="#0">Widgets</a>
+      <ol class="sub-menu">
+        <li class="menu-item"><a href="#0">Big Widgets</a></li>
+        <li class="menu-item"><a href="#0">Bigger Widgets</a></li>
+        <li class="menu-item"><a href="#0">Huge Widgets</a></li>
+      </ol>
+    </li>
+    <li class="menu-item">
+      <a href="#0">Kabobs</a>
+      <ol class="sub-menu">
+        <li class="menu-item"><a href="#0">Shishkabobs</a></li>
+        <li class="menu-item"><a href="#0">BBQ kabobs</a></li>
+        <li class="menu-item"><a href="#0">Summer kabobs</a></li>
+      </ol>
+    </li>
+    <li class="menu-item"><a href="#0">Contact</a></li>
+  </ol>
+</nav>
 <h1>WELCOME TO MOVIEAPP</h1>
 <p></p>
 <h3>Global Top 10</h3> 
