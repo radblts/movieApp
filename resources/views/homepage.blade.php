903ae99c5311ffcd989643b210afdd6d744548a1
DxniChid <chidiroglou.danai06@gmail.com>
2025-02-05 10:43:31 +0100

    .

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> MovieHopper - Home </title>
+    <link rel="stylesheet" href="{{url(path: 'css/homepage.css')}}"></link>
+        
+</head>
+<body>
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#contact">Contact</a>
+  <a href="#login">Login</a>
+  <div class="dropdown">
+    <button class="dropbtn">Genres
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+    <a href="#">Action</a>  
+    <a href="#">Fantasy</a>
+      <a href="#">For children</a>
+      <a href="#">Horror</a>
+    </div>
+  </div>
+</div>
+
+
+
+
+<h1>WELCOME TO MOVIEHOPPER</h1>
+<p></p>
+<h3>Global Top 10</h3> 
+
+
+
+<h3>************ movie caroussel here*************</h3>
+
+<section class="footer">
+      <div class="footer-row">
+        <div class="footer-col">
+          <h4>General</h4>
+          <ul class="links">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Idea</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Collection</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Legal</h4>
+          <ul class="links">
+            <li><a href="#">Customer Agreement</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <form action="#">
+            <input type="text" placeholder="Your email" required>
+            <button type="submit">SUBSCRIBE</button>
+          </form>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+    
+</body>
+</html>
