66b8099c3c3196222aa70617250955ca5010e807
DxniChid <chidiroglou.danai06@gmail.com>
2025-02-05 08:44:44 +0100

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,23 +15,23 @@
     <li class="menu-item"><a href="#0">About</a></li>
     <li class="menu-item">
       <a href="#0">Widgets</a>
-      <ol class="sub-menu">
-        <li class="menu-item"><a href="#0">Big Widgets</a></li>
-        <li class="menu-item"><a href="#0">Bigger Widgets</a></li>
-        <li class="menu-item"><a href="#0">Huge Widgets</a></li>
-      </ol>
     </li>
     <li class="menu-item">
-      <a href="#0">Kabobs</a>
+      <a href="#0">Genres</a>
       <ol class="sub-menu">
-        <li class="menu-item"><a href="#0">Shishkabobs</a></li>
-        <li class="menu-item"><a href="#0">BBQ kabobs</a></li>
-        <li class="menu-item"><a href="#0">Summer kabobs</a></li>
+        <li class="menu-item"><a href="#0">Horror</a></li>
+        <li class="menu-item"><a href="#0">Fantasy</a></li>
+        <li class="menu-item"><a href="#0">For children</a></li>
+        <li class="menu-item"><a href="#0">Action</a></li>
+        <li class="menu-item"><a href="#0">Documentaries</a></li>
       </ol>
     </li>
     <li class="menu-item"><a href="#0">Contact</a></li>
   </ol>
 </nav>
+
+
+
 <h1>WELCOME TO MOVIEAPP</h1>
 <p></p>
 <h3>Global Top 10</h3> 
