2a1e710b6109bec2ce03830379896cd3ec7e41ae
radblts <nevaroten@gmail.com>
2025-02-12 10:00:13 +0100

    start the login system
    https://youtu.be/7uzvrB0pOYs?si=FaqkOjH1c2yo0iKd
    
    27:41

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,31 @@
+@extends("layouts.default")
+@section("title", "Login")
+@section("content")
+<div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card p-4 shadow" style="width: 300px;">
+            <h4 class="text-center mb-3">Login</h4>
+            <form method="POST" action="{{route("login.post")}}">
+                @csrf
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" required>
+                    @if ($errors->has('email'))
+                        <span class="text-danger">
+                            {{$errors->first('email')}}
+                        </span>
+                    @endif
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Enter password" required>
+                    @if ($errors->has('password'))
+                        <span class="text-danger">
+                            {{$errors->first('password')}}
+                        </span>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-primary w-100">Login</button>
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, "Login"]);;
+Route::post("/login", [AuthController::class,"loginPost"])
+->name("login.posts");
+Route::get("/register", [AuthController::class, "register"]);
+Route::post("/login", [AuthController::class,"registerPost"])
+->name("register.post");
