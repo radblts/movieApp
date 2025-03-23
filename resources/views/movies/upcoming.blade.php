63d21bd263f1e56673d44088099ff5b941260d1a
radblts <nevaroten@gmail.com>
2025-03-23 22:58:53 +0100

    added navigation which i forgot previously

--- resources/views/movies/upcoming.blade.php
+++ resources/views/movies/upcoming.blade.php
@@ -4,6 +4,32 @@
 
 <link rel="stylesheet" href="{{ url('css/homepage.css') }}">
 
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark w-100">
+    <a class="navbar-brand px-3" href="#">
+      <img src="{{ url('imgs/logo.png') }}" alt="Logo">
+      MovieHopper
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ route('upcomingMovies') }}">Upcoming Movies</a></li>
+            <li>
+                @if(Auth::check())
+                <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                @csrf
+                <button type="submit" class="btn btn-danger">Logout</button>
+                </form>
+                @endif
+            </li>
+        </ul>
+    </div>
+</nav>
+
 <div class="container">
     <header class="text-center mt-4">
         <h1>Upcoming Movies</h1>
